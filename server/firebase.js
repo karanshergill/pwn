f0620c59e16cd0257d8526657846fa91c8a76b35
Karan <email@karanshergill.com>
2025-03-31 22:27:12 +0530

    disable firestore debugging

--- server/firebase.js
+++ server/firebase.js
@@ -13,17 +13,17 @@ admin.initializeApp({
 const db = admin.firestore();
 
 // test firestore connection
-db.collection('test-collection').limit(1).get()
-    .then(snapshot => {
-        if (snapshot.empty) {
-            console.log('Connected to Firestore: No documents found in test-collection');
-        } else {
-            console.log('Connected to Firestore: Documents retrieved');
-        }
-    })
-    .catch(error => {
-        console.error('Error connecting to Firestore:', error);
-    });
+// db.collection('test-collection').limit(1).get()
+//     .then(snapshot => {
+//         if (snapshot.empty) {
+//             console.log('Connected to Firestore: No documents found in test-collection');
+//         } else {
+//             console.log('Connected to Firestore: Documents retrieved');
+//         }
+//     })
+//     .catch(error => {
+//         console.error('Error connecting to Firestore:', error);
+//     });
 
 // export firestore instance
 export { db };
